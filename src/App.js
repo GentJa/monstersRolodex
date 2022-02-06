76a7db4b5bbb58ecf0169d245ff3e7c693167f37
Gent Jashari <gentjashari1997@gmail.com>
2022-02-06 21:40:28 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list-component';
+import { SearchBox } from './components/search-box/search-box-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state =  {
+      monster: [],
+      searchField:''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(user => this.setState({monster: user}))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField:e.target.value});
+  }
+
+  render() {
+    const {monster, searchField } = this.state;
+    const filteredMonsters = monster.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        {/* <h1>{this.state.name}</h1> */}
+        {/* <button onClick={() => this.setState({name:'Hello Gent'})}>Change Text</button> */}
+        {/* <input type="search" 
+         placeholder='search'
+         onChange={e => this.setState({searchField:e.target.value})}/> */}
+
+         <SearchBox 
+          placeholder='search monsters by name'
+          handleChange={this.handleChange}
+         />
+        <CardList monster = {filteredMonsters}></CardList>
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
